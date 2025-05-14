dc07875803f0f641b87c5a9246c481af3b249546
Zero-27k <kevincordova31@gmail.com>
2025-05-14 12:19:38 -0400

    Add files via upload

---
+++ inventario_computacion/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': "Gestión de Inventario de Equipos de Computación",
+    'version': '1.0',
+    'summary': "Módulo para gestionar el inventario de laptops, PCs y componentes.",
+    'description': """
+        Este módulo permite gestionar el inventario de equipos de computación,
+        incluyendo laptops, PCs y componentes, con seguimiento de ubicación y seriales únicos.
+    """,
+    'author': "Tu Nombre",
+    'website': "tu_sitio_web.com",
+    'category': 'Inventory',
+    'depends': ['stock'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/equipo_computo_views.xml',
+        'reports/report_equipo_computo.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
